6e4e4daff31d24e17fc5a9f9170fe28d910eade1
Chester Lynn <chester.htooaunglinn@gmail.com>
2020-12-18 19:53:24 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const api = {
+    key: "7c78c50a08486496c804d6f5f4b7b440",
+    url: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const getWeather = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href = "https://github.com/halchester/Weather-App">
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpngimg.com%2Fuploads%2Fgithub%2Fgithub_PNG40.png&f=1&nofb=1"
+        alt=""
+        className="github__logo"
+      />
+      </a>
+      <div
+        className={
+          typeof weather.main != "undefined"
+            ? weather.main.temp > 15
+              ? "weather__container warm"
+              : "weather__container"
+            : "weather__container"
+        }
+      >
+        <h1>Weather App</h1>
+        <h5>Enter your location</h5>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={getWeather}
+        ></input>
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <div className="location__box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather__box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
